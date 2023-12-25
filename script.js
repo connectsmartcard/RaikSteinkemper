7fad18a897dfcb49a425b78aa5dcdc36f7580733
connectsmartcard <154767623+connectsmartcard@users.noreply.github.com>
2023-12-25 02:03:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,51 +2,39 @@ document.addEventListener('DOMContentLoaded', (event) => {
   const body = document.body;
   const themeToggleDark = document.getElementById('theme-toggle-dark');
   const themeToggleLight = document.getElementById('theme-toggle-light');
-  const downloadButton = document.getElementById('download-pdf');
+  // Überprüfen, ob das Element existiert, bevor der Event Listener hinzugefügt wird
+const downloadButton = document.getElementById('download-pdf');
+if (downloadButton) {
+  downloadButton.addEventListener('click', function() {
+    const cardElement = document.querySelector('.card');
+    const scale = window.devicePixelRatio; // Geräte-Pixel-Ratio für eine hohe Auflösung
+    const width = cardElement.offsetWidth * scale;
+    const height = cardElement.offsetHeight * scale;
 
-
-  if (downloadButton) {
-    downloadButton.addEventListener('click', function() {
-
-      html2canvas(document.querySelector('.card'), { 
-        scale: 2, // Anpassung der Skalierung für bessere Qualität, damals 2
-        useCORS: true
-      }).then(canvas => {
-        const imgData = canvas.toDataURL('image/png', 1.0);
-        const pdf = new jspdf.jsPDF({
-
-           orientation: canvas.width > canvas.height ? 'landscape' : 'portrait',
-           unit: 'mm',
-           format: 'a6'
-
-          // orientation: 'landscape',
-          // unit: 'px',
-          // format: [canvas.width / scale, canvas.height / scale]
-        });
-  
-        // Berechnen Sie das Seitenverhältnis des Bildes
-        const canvasAspectRatio = canvas.width / canvas.height;
-        const a4AspectRatio = pdf.internal.pageSize.getWidth() / pdf.internal.pageSize.getHeight();
-        let imgWidth = pdf.internal.pageSize.getWidth();
-        let imgHeight = pdf.internal.pageSize.getHeight();
-        
-        // Anpassung der Bildgröße, um Verzerrung zu vermeiden
-        if (canvasAspectRatio > a4AspectRatio) {
-          // Canvas ist breiter als A4
-          imgHeight = imgWidth / canvasAspectRatio;
-        } else {
-          // Canvas ist höher als A4
-          imgWidth = imgHeight * canvasAspectRatio;
-        }
-  
-        const x = (pdf.internal.pageSize.getWidth() - imgWidth) / 2;
-        const y = (pdf.internal.pageSize.getHeight() - imgHeight) / 2;
-  
-        pdf.addImage(imgData, 'PNG', x, y, imgWidth, imgHeight);
-        pdf.save('visitenkarte.pdf');
+    html2canvas(cardElement, {
+      scale: scale,
+      width: width,
+      height: height,
+      useCORS: true, // Erlauben Sie das Laden von Ressourcen über Domains hinweg
+      // ...weitere Optionen...
+    }).then(canvas => {
+      // Erstellen Sie das PDF mit den Maßen des Canvas
+      const pdf = new jsPDF({
+        orientation: 'p', // 'p' für Portrait, 'l' für Landscape
+        unit: 'px',
+        format: [width, height],
       });
+
+      // Fügen Sie das gerenderte Bild dem PDF hinzu
+      pdf.addImage(canvas.toDataURL('image/png'), 'PNG', 0, 0, width, height);
+      pdf.save('visitenkarte.pdf');
     });
-  } 
+  });
+} else {
+  // Fehlerbehandlung, falls der Button nicht gefunden wurde
+  console.error('Download-Button wurde nicht im DOM gefunden.');
+}
+
 
   // Funktion, um den Dark Mode zu aktivieren
   function enableDarkMode() {
